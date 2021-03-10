547cb67427de754f46638f4158430f4d4b9e1d0a
Akshaj Srinivasan <akshajsrinivasan99@gmail.com>
2021-03-09 19:03:12 -0800

    changes to scripts

--- analysis/Scripts/project_functions.py
+++ analysis/Scripts/project_functions.py
@@ -7,6 +7,20 @@ def load_and_process(url_or_path_to_csv_file):
         .rename(columns={'FISCAL_YEAR':'year', 'HEALTH_AUTHORITY': 'h_a', 'HOSPITAL_NAME':'hosp', 'PROCEDURE_GROUP': 'prcd', 'WAITING':'waiting', 'COMPLETED': 'comp', 'COMPLETED_50TH_PERCENTILE': 'wait_med', 'COMPLETED_90TH_PERCENTILE':'wait_90'})
         .dropna()
 )
+    
+    wdata['waiting'] = (
+                  wdata['waiting'].str.replace(',','')
+                                .str.replace('<5','3')
+)
+
+    wdata['comp'] = (
+               wdata['comp'].str.replace(',','')
+                          .str.replace('<5','3')
+)
+
+    wdata['waiting'] = pd.to_numeric(wdata['waiting'])
+    wdata['comp'] = pd.to_numeric(wdata['comp'])
+    
 
     ddata = wdata.assign(wait_median_d = wdata['wait_med']*7)
     ddata = ddata.assign(wait_90_d = wdata['wait_90']*7)
