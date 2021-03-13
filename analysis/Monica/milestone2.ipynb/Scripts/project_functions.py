1c7d6cdf5d4cafc09cd4d4df4abff755d116ca49
Akshaj Srinivasan <akshajsrinivasan99@gmail.com>
2021-03-12 22:59:44 -0800

    Updated Scripts file. Completed EDA

--- analysis/Monica/milestone2.ipynb/Scripts/project_functions.py
+++ analysis/Monica/milestone2.ipynb/Scripts/project_functions.py
@@ -28,3 +28,29 @@ def load_and_process(url_or_path_to_csv_file):
     return ddata
 
 
+
+def main_Allsummary_data(wd):
+    wd_all = wd[(wd['h_a'] == 'All Health Authorities') & (wd['prcd'] == 'All Procedures')]
+    return wd_all
+
+
+
+def basic_data(wd):
+    wd = wd[~(wd['h_a'] == 'All Health Authorities')] 
+    wd = wd[~(wd['hosp'] == 'All Facilities')] 
+    wd = wd[~(wd['prcd'] == 'All Procedures')]
+    return wd
+
+
+
+def all_procd(wd):
+    wd_all_prcd = wd[(wd['prcd'] == 'All Procedures')]
+    wd_all_prcd = wd_all_prcd[~(wd_all_prcd['hosp'] == 'All Facilities')] 
+    return wd_all_prcd
+    
+    
+    
+def all_h_a:
+    wd_all_ha = wd[(wd['h_a'] == 'All Health Authorities')]
+    wd_all_ha = wd_all_ha[~(wd_all_ha['prcd'] == 'All Procedures')]
+    return wd_all_ha
